dfd546b388f53df27d4e1aeecf86264533405766
Saniya Baghirova <saniya.ibraimova@gmail.com>
2021-10-01 15:08:19 +0200

    Out of memory exeption

---
+++ Innlevering 2/src/main/java/no/kristiania/http/HttpClient.java
@@ -0,0 +1,54 @@
+package no.kristiania.http;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.sql.PreparedStatement;
+
+public class HttpClient {
+    private final int statusCode;
+
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+
+        Socket socket = new Socket(host, port);
+
+        //request to server
+        String request = "GET " + requestTarget + "HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                "Connection: close\r\n"  + //sier til serveren at vi skal gjøre bare 1 requsest
+                "\r\n";
+        socket.getOutputStream().write(request.getBytes());
+
+        //skal lese og holde første linje i response headers
+        String[] statusLine = readLine(socket.getInputStream()).split(" ");
+        this.statusCode = Integer.parseInt(statusLine[1]);//tar imot value under index 1 i status
+
+
+    }
+
+    private String readLine(InputStream in) throws IOException {
+        //skal bygge opp string gradvis
+        StringBuilder result = new StringBuilder();
+
+        //while det kommer på slutten av linje '\r'
+        int c;
+        while ((c = in.read()) != -1 && c != '\r') {
+            result.append((char)c);
+        }
+        if (c == '\r') {
+            in.read();
+        }
+
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        System.out.println(client.getStatusCode());
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
--- Innlevering 2/src/test/java/no/kristiania/http/HttpClientTest.java
+++ Innlevering 2/src/test/java/no/kristiania/http/HttpClientTest.java
@@ -1,21 +1,20 @@
+package no.kristiania.http;
+
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.net.http.HttpClient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
-
     @Test
-    void shouldReturnStatusCode() throws IOException {
+    public void shouldReturnStatusCode() throws IOException {
         assertEquals(200,
                 new HttpClient("httpbin.org", 80, "/html")
                         .getStatusCode());
         assertEquals(404,
-                new HttpClient("httpbin.org", 80, "/no-such-page")
+                new HttpClient("httpbin.org", 80, "/html")
                         .getStatusCode());
+
     }
-
-
 }

8e9091503b0cb974892517542c42041e4022a3d3
Saniya Baghirova <saniya.ibraimova@gmail.com>
2021-10-01 14:21:03 +0200

    First test to check Maven

---
+++ Innlevering 2/src/test/java/no/kristiania/http/HttpClientTest.java
@@ -0,0 +1,13 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+
+    @Test
+    void firstTest() {
+        assertEquals(100, 100);
+    }
+}

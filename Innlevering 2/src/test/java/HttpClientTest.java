bfe6b53c6cc89d5483459e314a9494631dab78c9
Marina1014 <makh004@student.kristiania.no>
2021-10-01 14:35:56 +0200

    failing test

---
+++ Innlevering 2/src/test/java/HttpClientTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+
+    @Test
+    void shouldReturnStatusCode() throws IOException {
+        assertEquals(200,
+                new HttpClient("httpbin.org", 80, "/html")
+                        .getStatusCode());
+        assertEquals(404,
+                new HttpClient("httpbin.org", 80, "/no-such-page")
+                        .getStatusCode());
+    }
+}

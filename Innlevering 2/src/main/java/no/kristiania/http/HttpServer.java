69606b49000598e9b3f6bad6a83c5d77515eded2
Marina1014 <makh004@student.kristiania.no>
2021-10-02 10:36:02 +0200

    Passing server test🟢

--- Innlevering 2/src/main/java/no/kristiania/http/HttpServer.java
+++ Innlevering 2/src/main/java/no/kristiania/http/HttpServer.java
@@ -1,7 +1,9 @@
 package no.kristiania.http;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class HttpServer {
@@ -11,16 +13,82 @@ public class HttpServer {
 
     public HttpServer(int serverPort) throws IOException {
         serverSocket = new ServerSocket(serverPort);
-
+        new Thread(this::handleClients).start();
 
     }
+
 
     private void handleClients() {
+        try {
+            //setter sammen klient og server i en socket
+            Socket client = serverSocket.accept();
+
+            String[] requestLine = HttpClient.readLine(client).split(" ");
+            String requestTarget = requestLine[1];
+
+            // indexOf() vil returnere -1 da den ikke finner indeksen til "?"
+            // fileTarget blir requestLine[1] fra posisjon 0 til "?"
+            // query er resterende del av requestLine[1] fra "?"
+            int questionPos = requestTarget.indexOf('?');
+            String fileTarget;
+            String query = null;
+            if (questionPos != -1) {
+                fileTarget = requestTarget.substring(0, questionPos);
+                query = requestTarget.substring(questionPos + 1);
+            } else {
+                fileTarget = requestTarget;
+            }
+
+            if (fileTarget.equals("/hello")) {
+                String yourName = "world";
+                if (query != null) {
+                    yourName = query.split("=")[1];
+                }
+                String responseText = "<p>Hello " + yourName + "</p>";
+
+                String response = "HTTP/1.1 200 OK\r\n" +
+                        "Content-Length: " + responseText.length() + "\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "\r\n" +
+                        responseText;
+                client.getOutputStream().write(response.getBytes());
+            } else {
+                if (rootDirectory != null && Files.exists(rootDirectory.resolve(fileTarget.substring(1)))) {
+                    String responseText = Files.readString(rootDirectory.resolve(fileTarget.substring(1)));
+
+                    String contentType = "text/plain";
+                    if (requestTarget.endsWith(".html")) {
+                        contentType = "text/html";
+                    }
+                    String response = "HTTP/1.1 200 OK\r\n" +
+                            "Content-Length: " + responseText.length() + "\r\n" +
+                            "Content-Type: " + contentType + "\r\n" +
+                            "\r\n" +
+                            responseText;
+                    client.getOutputStream().write(response.getBytes());
+                    return;
+                }
 
 
+                String responseText = "File not found: " + requestTarget;
+
+                String response = "HTTP/1.1 404 Not found\r\n" +
+                        "Content-Length: " + responseText.length() + "\r\n" +
+                        "\r\n" +
+                        responseText;
+                client.getOutputStream().write(response.getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    public int getPort() {
+        return serverSocket.getLocalPort();
+    }
+}
 
+/*
     public static void main(String[] args) throws IOException {
         //klassen brukes for å lage server
         ServerSocket serverSocket = new ServerSocket(8080);
@@ -49,6 +117,8 @@ public class HttpServer {
             client.getOutputStream().write(responseMessage.getBytes());
         }
     }
-}
+
+ */
+
 
 

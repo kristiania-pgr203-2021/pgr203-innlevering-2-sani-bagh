c4cce8da86eb9b6466cae295a88be27efcbc409f
Marina1014 <makh004@student.kristiania.no>
2021-10-03 10:48:16 +0200

    post product

---
+++ Innlevering 2/src/main/java/no/kristiania/http/Product.java
@@ -0,0 +1,17 @@
+package no.kristiania.http;
+
+public class Product {
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String firstName) {
+        this.name = firstName;
+    }
+
+
+}
